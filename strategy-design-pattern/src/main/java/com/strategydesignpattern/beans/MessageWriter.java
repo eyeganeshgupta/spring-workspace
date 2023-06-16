b3f10997b05e9892f8acecb161632839be334bff
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-06-16 08:11:23 +0530

    requirement-changed-from-html-to-pdf

--- strategy-design-pattern/src/main/java/com/strategydesignpattern/beans/MessageWriter.java
+++ strategy-design-pattern/src/main/java/com/strategydesignpattern/beans/MessageWriter.java
@@ -2,6 +2,7 @@ package com.strategydesignpattern.beans;
 
 import com.strategydesignpattern.converters.HTMLMessageConverterImpl;
 import com.strategydesignpattern.converters.IMessageConverter;
+import com.strategydesignpattern.converters.PDFMessageConverterImpl;
 
 public class MessageWriter {
 	private IMessageConverter messageConverter;
@@ -9,7 +10,7 @@ public class MessageWriter {
 	public void writeMessage(String message) {
 		String convertedMessage = null;
 
-		messageConverter = new HTMLMessageConverterImpl();
+		messageConverter = new PDFMessageConverterImpl();
 		convertedMessage = messageConverter.convert(message);
 
 		System.out.println(convertedMessage);
