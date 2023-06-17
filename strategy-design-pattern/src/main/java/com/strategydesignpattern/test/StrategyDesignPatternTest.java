1f6549e994d4597f36b0879e6513bdcd2efcc76f
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-06-17 20:52:03 +0530

    Dependency Injection

--- strategy-design-pattern/src/main/java/com/strategydesignpattern/test/StrategyDesignPatternTest.java
+++ strategy-design-pattern/src/main/java/com/strategydesignpattern/test/StrategyDesignPatternTest.java
@@ -1,10 +1,17 @@
 package com.strategydesignpattern.test;
 
 import com.strategydesignpattern.beans.MessageWriter;
+import com.strategydesignpattern.converters.IMessageConverter;
+import com.strategydesignpattern.converters.*;
 
 public class StrategyDesignPatternTest {
 	public static void main(String[] args) {
 		MessageWriter messageWriter = new MessageWriter();
+		IMessageConverter messageConverter = new HTMLMessageConverterImpl();
+		
+		// The process of passing the dependent object into another class is called Dependency Injection
+		messageWriter.setMessageConverter(messageConverter);
+		
 		messageWriter.writeMessage("Strategy Design Pattern");
 	}
 }
