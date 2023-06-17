235068c113a0d1d7076fabf216c741e05f70d406
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-06-17 20:45:46 +0530

    don't create or pull the object

--- strategy-design-pattern/src/main/java/com/strategydesignpattern/beans/MessageWriter.java
+++ strategy-design-pattern/src/main/java/com/strategydesignpattern/beans/MessageWriter.java
@@ -1,18 +1,26 @@
 package com.strategydesignpattern.beans;
 
 import com.strategydesignpattern.converters.IMessageConverter;
-import com.strategydesignpattern.helper.MessageConverterFactory;
+// import com.strategydesignpattern.helper.MessageConverterFactory;
 
 public class MessageWriter {
 	private IMessageConverter messageConverter;
 
 	public void writeMessage(String message) {
 		String convertedMessage = null;
-
+		
+		/*
 		// dependency-pulling === going-and-getting-the-dependent-object-from-another-class
 		messageConverter = MessageConverterFactory.createMessageConverter("html");
+		*/
+		
+		// don't create or pull the object
 		convertedMessage = messageConverter.convert(message);
 
 		System.out.println(convertedMessage);
+	}
+	
+	public void setMessageConverter(IMessageConverter messageConverter) {
+		this.messageConverter = messageConverter;
 	}
 }
