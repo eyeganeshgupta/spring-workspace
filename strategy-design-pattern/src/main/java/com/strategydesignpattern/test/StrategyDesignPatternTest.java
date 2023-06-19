5a3a9ca630d4c1ad830b0b5be231e75d90ac8da0
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-06-19 12:17:52 +0530

    getting-object-from-ObjectFactory

--- strategy-design-pattern/src/main/java/com/strategydesignpattern/test/StrategyDesignPatternTest.java
+++ strategy-design-pattern/src/main/java/com/strategydesignpattern/test/StrategyDesignPatternTest.java
@@ -2,16 +2,16 @@ package com.strategydesignpattern.test;
 
 import com.strategydesignpattern.beans.MessageWriter;
 import com.strategydesignpattern.converters.IMessageConverter;
-import com.strategydesignpattern.converters.*;
+import com.strategydesignpattern.helper.ObjectFactory;
 
 public class StrategyDesignPatternTest {
-	public static void main(String[] args) {
-		MessageWriter messageWriter = new MessageWriter();
-		IMessageConverter messageConverter = new HTMLMessageConverterImpl();
-		
+	public static void main(String[] args) throws Exception {
+		MessageWriter messageWriter = (MessageWriter) ObjectFactory.createObject("messageWriter.class");
+		IMessageConverter messageConverter = (IMessageConverter) ObjectFactory.createObject("messageConverter.class");
+
 		// The process of passing the dependent object into another class is called Dependency Injection
 		messageWriter.setMessageConverter(messageConverter);
-		
+
 		messageWriter.writeMessage("Strategy Design Pattern");
 	}
 }
