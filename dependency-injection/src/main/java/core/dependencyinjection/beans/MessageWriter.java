b052bfd6e3f7acf4cafd75275b0a1ec86dcfd04a
eyeganeshgupta <eyeganeshgupta@gmail.com>
2023-06-21 23:44:16 +0530

    MessageWriter

--- dependency-injection/src/main/java/core/dependencyinjection/beans/MessageWriter.java
+++ dependency-injection/src/main/java/core/dependencyinjection/beans/MessageWriter.java
@@ -1,5 +1,19 @@
 package core.dependencyinjection.beans;
 
-public class MessageWriter {
+import core.dependencyinjection.converters.IMessageConverter;
 
+public class MessageWriter {
+	private IMessageConverter messageConverter;
+
+	public void writeMessage(String message) {
+		String convertedMessage = null;
+
+		convertedMessage = messageConverter.convert(convertedMessage);
+
+		System.out.println(convertedMessage);
+	}
+
+	public void setMessageConverter(IMessageConverter messageConverter) {
+		this.messageConverter = messageConverter;
+	}
 }
